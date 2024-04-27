cf9e9a54255f534aa691d5fd786cf28cf6837d97
Evan Bourgoine <evanbourgoine@Evans-Mac-mini.local>
2024-04-27 18:59:05 -0400

    cleaned code up, added documentation

--- pass.py
+++ pass.py
@@ -3,7 +3,18 @@ import hashlib, binascii
 import time
 from itertools import combinations, permutations
 
-#create hash definition
+#Evan Bourgoine (ebourgoine25)
+#2104 Class Project
+#Option B: Dictionary Attack using different hash algorithms
+
+#Step 1: Read Dictionary
+#Step 2: User inputs
+#Step 3: Hash user input with different SHA256 - SHA512
+#Step 4: Perform dictionary attack
+#Step 5: Measure time it takes to guess password
+#Step 6: Present results in a graphical way
+
+#hash the user password using SHA256
 def hash256(password):
     hashed_pwd = hashlib.pbkdf2_hmac('sha256', #later you will changes this to sha512
                          password.encode('utf-8'),# passowrd is encoded into binary  
@@ -11,6 +22,7 @@ def hash256(password):
                          100000) # number of iterations ('resolution') of the hashing computation)
     return binascii.hexlify(hashed_pwd)# converting binary to hex 
 
+#hash the user password using SHA512
 def hash512(password):
     hashed_pwd = hashlib.pbkdf2_hmac('sha512', #later you will changes this to sha512
                          password.encode('utf-8'),# passowrd is encoded into binary  
@@ -18,6 +30,8 @@ def hash512(password):
                          100000) # number of iterations ('resolution') of the hashing computation)
     return binascii.hexlify(hashed_pwd)# converting binary to hex 
 
+#check if user password is in the dictionary, or combination of dictionary words
+#uses recursion to check combination of words
 def check_validity(password, dictionary):
     if password in dictionary:
         return True
@@ -41,6 +55,7 @@ def dict_attack256(hash_password, combo_dict):
         else:
             count = count + 1
 
+#perform attack using the SHA512 hashed password
 def dict_attack512(hash_password, combo_dict):
     count = 1
     for word in combo_dict:
@@ -50,6 +65,7 @@ def dict_attack512(hash_password, combo_dict):
         else:
             count = count + 1
 
+#will eliminate words in the dictionary if they are not the correct length 
 def eliminate_words(password, dictionary):
     updated_dictionary = []
     for word in dictionary:
@@ -57,7 +73,7 @@ def eliminate_words(password, dictionary):
             updated_dictionary.append(word)
     return updated_dictionary
     
-    
+#will generate all possible combinations of up to 3 words in the dictionary
 def generate_combinations(dictionary):
     combos = []
     for i in range(1, 4):
@@ -65,15 +81,6 @@ def generate_combinations(dictionary):
             for perm in permutations(combo):
                 combos.append(''.join(perm))
     return combos
-        
-#Step 1: Read Dictionary
-#Step 2: User inputs
-#Step 3: Hash user input with different SHA256 - SHA512
-#Step 4: Perform dictionary attack
-#Step 5: Measure time it takes to guess password
-#total_time = time.time() - start
-#print("Time Elapsed: ",total_time,"seconds")
-#Step 6: Present results in a graphical way
     
 def main():
 
@@ -96,29 +103,38 @@ def main():
             is_valid = True
         else:
             is_valid = False
-            print("Invalid password to check.")
+            print("Invalid password to check.") #program will not attempt to crack in invalid password
+
         #If password given is valid, perform dict. attack to find password in dictionary
         if is_valid: 
+            #hash user password to SHA256
             hashed_password256 = hash256(password)
             print("\nSHA256: ", hashed_password256)
+            
+            #hash user password to SHA512
             hashed_password512 = hash512(password)
             print("SHA512: ", hashed_password512, "\n")
-            combo_dict = generate_combinations(dict_array)
 
+            #generate all possible combinations of dictionary words that the password could be
+            combo_dict = generate_combinations(dict_array)
+            #elimate passwords that are not the correct length
             updated_dict = eliminate_words(password, combo_dict)
 
+            #begin SHA256 attack, track time
             start256 = time.time()
             dict_attack256(hashed_password256, updated_dict)
             end256 = time.time()
 
+            #begin SHA512 attack, track time
             start512 = time.time()
             dict_attack512(hashed_password512, updated_dict)
             end512 = time.time()
 
+            #print time it takes to crack both attacks
             print("Time to crack SHA256: ", end256 - start256)
             print("Time to crack SHA512: ", end512 - start512)
     
-        password = input("Enter password: \t")
+        password = input("Enter password: \t") #prompt for anothr password, or 'q'
     
         
 
